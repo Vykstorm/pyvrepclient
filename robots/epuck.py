d46a0293ebd7aa77099ee9c419ad99f3e2f7da30
Vykstorm <victorruizgomezdev@gmail.com>
2018-01-09 13:35:17 +0100

    Modificado el orden de los sensores de proximidad para el robot e-puck

--- robots/epuck.py
+++ robots/epuck.py
@@ -16,14 +16,14 @@ class EPuck(ObjectsCollection):
 
     # Sensores de proximidad
     proximity_sensors = [
-        'ePuck_proxSensor1',
-        'ePuck_proxSensor2',
-        'ePuck_proxSensor3',
-        'ePuck_proxSensor4',
-        'ePuck_proxSensor5',
-        'ePuck_proxSensor6',
-        'ePuck_proxSensor7',
-        'ePuck_proxSensor8'
+        'ePuck_proxSensor4', # IR 0
+        'ePuck_proxSensor5', # IR 1
+        'ePuck_proxSensor6', # IR 2
+        'ePuck_proxSensor7', # IR 3
+        'ePuck_proxSensor8', # IR 4
+        'ePuck_proxSensor1', # IR 5
+        'ePuck_proxSensor2', # IR 6
+        'ePuck_proxSensor3'  # IR 7
     ]
 
     # Sensores de visión
@@ -46,23 +46,14 @@ class EPuck(ObjectsCollection):
         self.camera = self.vision_sensors.camera
         self.light_sensor = self.vision_sensors.light_sensor
 
-        self.prox_sensors15 = Namespace(left = self.proximity_sensors[2],
-                                        right = self.proximity_sensors[3])
-        self.prox_sensors45 = Namespace(left = self.proximity_sensors[1],
-                                        right = self.proximity_sensors[4])
-        self.prox_sensors90 = Namespace(left = self.proximity_sensors[0],
-                                        right = self.proximity_sensors[5])
-        self.prox_sensors135 = Namespace(left = self.proximity_sensors[7],
-                                         right = self.proximity_sensors[6])
-
-        self.prox_sensor15 = self.proximity_sensors[3]
-        self.prox_sensor45 = self.proximity_sensors[4]
-        self.prox_sensor90 = self.proximity_sensors[5]
-        self.prox_sensor135 = self.proximity_sensors[6]
-        self.prox_sensor225 = self.proximity_sensors[7]
-        self.prox_sensor270 = self.proximity_sensors[0]
-        self.prox_sensor315 = self.proximity_sensors[1]
-        self.prox_sensor345 = self.proximity_sensors[2]
+        self.prox_sensor15 = self.proximity_sensors[0]
+        self.prox_sensor45 = self.proximity_sensors[1]
+        self.prox_sensor90 = self.proximity_sensors[2]
+        self.prox_sensor135 = self.proximity_sensors[3]
+        self.prox_sensor225 = self.proximity_sensors[4]
+        self.prox_sensor270 = self.proximity_sensors[5]
+        self.prox_sensor315 = self.proximity_sensors[6]
+        self.prox_sensor345 = self.proximity_sensors[7]
 
         self.motors = Namespace(left = self.joints.left_motor,
                                 right = self.joints.right_motor)
